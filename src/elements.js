a77d42e9e9e57ae691926918a2eb5c4e599f432c
jpmakinen <pullasuti87@gmail.com>
2023-12-11 22:53:46 +0200

    it works!

---
+++ src/elements.js
@@ -0,0 +1,51 @@
+const alkaliMetal = '#ffb4a2'
+const alkalineEarth = '#a5c7d4'
+const transitionMetal = '#f0c987'
+const basicMetal = '#c8a7d1'
+const semimetal = '#bbb9c5'
+const nonmetal = '#6decb9'
+const halogen = '#a0f4db '
+const nobleGas = '#db92b2'
+const lanthanide = '#ffd3b6'
+const actinide = 'e1c6d9'
+
+const elements = {
+    H: nonmetal,
+    He: nobleGas,
+    Li: alkaliMetal,
+    Be: alkalineEarth,
+    B: semimetal,
+    C: nonmetal,
+    N: nonmetal,
+    O: nonmetal,
+    F: halogen,
+    Ne: nobleGas, // 10
+    Na: alkaliMetal,
+    Mg: alkalineEarth,
+    Al: basicMetal,
+    Si: semimetal,
+    P: nonmetal,
+    S: nonmetal,
+    Cl: halogen,
+    Ar: nobleGas,
+    K: alkaliMetal,
+    Ca: alkalineEarth,
+    Sc: transitionMetal,
+    Ti: transitionMetal,
+    V: transitionMetal,
+    Cr:transitionMetal,
+    Mn: transitionMetal,
+    Fe: transitionMetal,
+    Co: transitionMetal,
+    Ni: transitionMetal,
+    Cu: transitionMetal,
+    Zn: transitionMetal,
+    Ga: basicMetal,
+    Ge: semimetal,
+    As:semimetal,
+    Se: nonmetal, 
+    Br: halogen,
+    Kr: nobleGas, // 36
+}
+
+export default elements
